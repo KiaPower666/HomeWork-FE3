1859a5bdc79e42b6bed99270258f5ccb3c8445f7
KiaPower666 <118475631+KiaPower666@users.noreply.github.com>
2023-01-30 17:08:21 +0300

    Add files via upload

---
+++ HomeWork3/script.js
@@ -0,0 +1,74 @@
+let hour = 1;
+let day = 24;
+
+let resulthour = hour * 60 * 60 
+let resultday = day * 60 * 60
+
+console.log(resulthour); // 3600
+console.log(resultday); // 86400
+// <------>
+let num = 3;
+
+alert(3)
+// <------>
+let a = 10;
+let b = 2;
+
+console.log(a+b); // 12
+console.log(a-b); // 8
+console.log(a*b); // 20
+console.log(a/b); // 5
+// <------>
+let c = 15;
+let d = 2;
+
+let result = c + d;
+console.log(result) // 17
+// <------>
+let a1 = 10;
+let b1 = 2;
+let c1 = 5;
+
+let result1 = a1 + b1 + c1;
+console.log(result1) // 17
+// <------>
+let a2 = 17;
+let b2 = 10;
+let c2 = a2 - b2;
+let d2 = 7;
+let result2 = a2 + b2 + c2;
+console.log(result2) // 34
+// <------>
+let str = 'Привет,Мир!';
+console.log(str)
+// <------>
+let str1 = 'Привет';
+let str2 = 'Мир!';
+console.log(str1 + ',' + str2)
+// <------>
+let name = 'Vlad';
+console.log('Привет ' + name)
+// <------>
+let age = '21';
+console.log('Мне ' + age + ' год!')
+// <------>
+let a3 = prompt()
+if (a3 == 0 || a3 == 1 || a3 == 0 || a3 == -3) {
+    alert("Верно")
+}else {
+    alert("Неверно")
+}
+// <------>
+let a4 = prompt()
+if (a4 > 0 || a4 > 1 || a4 > 0 || a4 > -3) {
+    alert("Верно")
+}else {
+    alert("Неверно")
+}
+// <------>
+let a5 = prompt()
+if (a5 < 0 || a5 < 1 || a5 < 0 || a5 < -3) {
+    alert("Верно")
+}else {
+    alert("Неверно")
+}
